e6f262d93509419867ce5e374f9b2c9281702cbb
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-23 10:51:22 -0500

    feat: added business logic to aggregate.

--- src/main/java/com/acme/sales/domain/model/aggregates/SalesOrder.java
+++ src/main/java/com/acme/sales/domain/model/aggregates/SalesOrder.java
@@ -16,24 +16,76 @@ public class SalesOrder {
         return shippingAddress.getAddressAsString();
     }
 
-    private final Address shippingAddress;
+    private Address shippingAddress;
 
     private SalesOrderStatus status;
 
     private List<SalesOrderItem> items;
 
+    private double paymentAmount;
 
-    public SalesOrder(Address shippingAddress) {
+
+    public SalesOrder() {
         this.internalId = UUID.randomUUID();
-        this.shippingAddress = shippingAddress;
         this.status = SalesOrderStatus.CREATED;
+        paymentAmount = 0.0;
+    }
+
+    public void dispatch(String street, String city, String state, String zipCode, String country) {
+        verifyIfReadyForDispatch();
+        this.shippingAddress = new Address(street, city, state, zipCode, country);
+        this.status = SalesOrderStatus.IN_PROGRESS;
+        this.items.forEach(SalesOrderItem::dispatch);
+
+    }
+
+    public boolean isDispatched() {
+        return this.items.stream().allMatch(SalesOrderItem::isDispatched);
+    }
+
+    public void completeDelivery() {
+        verifyIfItemsAreDispatched();
+        if (this.status == SalesOrderStatus.SHIPPED)
+            this.status = SalesOrderStatus.DELIVERED;
     }
 
     public void cancel() {
+        this.clearItems();
         this.status = SalesOrderStatus.CANCELLED;
     }
 
     public void addItem(int quantity, double unitPrice, Long productId) {
+        if (this.status == SalesOrderStatus.APPROVED)
+            throw new IllegalStateException("Cannot add items to an approved order");
         this.items.add(new SalesOrderItem(quantity, productId, unitPrice));
+    }
+
+    public double calculateTotalPrice() {
+        return items.stream().mapToDouble(SalesOrderItem::calculatePrice).sum();
+    }
+
+    public void addPayment(double amount) {
+        if (amount <= 0)
+            throw new IllegalArgumentException("Payment amount must be greater than 0");
+
+        if (amount > calculateTotalPrice() - paymentAmount)
+            throw new IllegalArgumentException("Payment amount exceeds the total price");
+
+        this.paymentAmount += amount;
+        verifyIfReadyForDispatch();
+    }
+
+    private void verifyIfReadyForDispatch() {
+        if (this.status == SalesOrderStatus.APPROVED) return;
+        if (paymentAmount == calculateTotalPrice()) this.status = SalesOrderStatus.APPROVED;
+    }
+
+    private void clearItems() {
+        this.items.clear();
+    }
+
+    public void verifyIfItemsAreDispatched() {
+        if (isDispatched())
+            this.status = SalesOrderStatus.SHIPPED;
     }
 }
--- src/main/java/com/acme/sales/domain/model/aggregates/SalesOrderItem.java
+++ src/main/java/com/acme/sales/domain/model/aggregates/SalesOrderItem.java
@@ -15,6 +15,16 @@ public class SalesOrderItem {
 
 
     public SalesOrderItem(int quantity, Long productId, double unitPrice) {
+
+        if (quantity == 0)
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+
+        if (productId == 0)
+            throw new IllegalArgumentException("Product ID must be greater than 0");
+
+        if (unitPrice <= 0)
+            throw new IllegalArgumentException("Unit price must be greater than 0");
+
         this.itemId = UUID.randomUUID();
         this.quantity = quantity;
         this.productId = productId;
